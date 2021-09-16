be0cd1f01a70dfe794791ee74b1d0252e43101c0
Justin Salér <justin.saler.r@gmail.com>
2021-09-16 19:23:52 +0200

    Add support for Cow Fire Experiment in main

--- main.py
+++ main.py
@@ -17,14 +17,22 @@ skeleton_fire_experiment = {
     "goals": [conditions.IsEnemyDefeated],
     "mission": "resources/arena_skeleton_v2.xml",
     "tree_log": "skeleton_tree.txt",
-    "hard_reset": True  # TODO: Add support for soft reset to experiments with fire
+    "hard_reset": True,  # TODO: Add support for soft reset to experiments with fire
+    "observation_filter": [
+        "entity_relative_position", "enemy_relative_position", "direction", "health", "enemy_health", "entity_visible",
+        "surroundings"
+    ]
 }
 
 cow_fire_experiment = {
     "goals": [conditions.IsNotInFire, conditions.IsNotHungry],
     "mission": "resources/arena_cow_v2.xml",
     "tree_log": "cow_tree.txt",
-    "hard_reset": True  # TODO: Add support for soft reset to experiments with fire
+    "hard_reset": True,  # TODO: Add support for soft reset to experiments with fire
+    "observation_filter": [
+        "entity_relative_position", "enemy_relative_position", "direction", "health", "entity_visible", "surroundings",
+        "is_entity_pickable", "has_food", "satiation"
+    ]
 }
 
 
@@ -44,6 +52,6 @@ def experiment_check_env(specs):
 
 
 if __name__ == '__main__':
-    # experiment_train(skeleton_fire_experiment)
-    experiment_test(cow_skeleton_experiment, "best_model_53")
+    # experiment_train(cow_fire_experiment)
+    experiment_test(cow_fire_experiment, "best_model_53")
     # experiment_check_env(cow_skeleton_experiment)

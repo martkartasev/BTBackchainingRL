ba896fde93a73f210e585eaad048ad1d64c4ad3b
Mart Kartasev <mart.kartasev@gmail.com>
2021-10-12 19:45:04 +0300

    ACC reward

--- learning/baseline_node.py
+++ learning/baseline_node.py
@@ -6,7 +6,7 @@ from bt.sequence import Sequence
 
 class BaselinesNode(Sequence):
     AGENT_DEAD_REWARD = -1000
-    ACC_VIOLATED_REWARD = -1000
+    ACC_VIOLATED_REWARD = -500
     POST_CONDITION_FULFILLED_REWARD = 1000
 
     def __init__(self, agent, name="A2CLearner", children=None, model=None, ):
